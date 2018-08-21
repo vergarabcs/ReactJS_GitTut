833e578aaa6b1efac2c9705f51ab38213b40fd9a
sagitaryux <gimeloven@gmail.com>
2018-08-21 13:31:01 +0800

    replaced nani.txt to a java file nani.java

---
+++ nani.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import javax.swing.*;
+
+class SongLyric{
+	public static void main(String[]args){
+		JOptionPane.showMessageDialog(null, "When I was a young boy my father took me into the city");
+	}
+}
